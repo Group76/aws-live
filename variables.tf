0490d9cbfe92d0cc46acceab6061bae28256c9a5
t-tozatto <thais.c.tozatto@gmail.com>
2024-05-09 20:46:52 -0300

    var

--- variables.tf
+++ variables.tf
@@ -17,52 +17,60 @@ variable "workspace" {
 variable "atlas_org_id" {
   type        = string
   description = "Atlas Organization ID"
+  default = "663b5bd2f5133b63245e8ff5"
 }
 # Atlas Project Name
 variable "atlas_project_name" {
   type        = string
   description = "Atlas Project Name"
+  default = "group76"
 }
 
 # Atlas Project Environment
 variable "environment" {
   type        = string
   description = "The environment to be built"
+  default = "dev"
 }
 
 # Cluster Instance Size Name 
 variable "cluster_instance_size_name" {
   type        = string
   description = "Cluster instance size name"
+  default = "M0"
 }
 
 # Cloud Provider to Host Atlas Cluster
 variable "cloud_provider" {
   type        = string
   description = "AWS or GCP or Azure"
+  default = "AWS"
 }
 
 # Atlas Region
 variable "atlas_region" {
   type        = string
   description = "Atlas region where resources will be created"
-  default = "us-east-2"
+  default = "US_EAST_2"
 }
 
 # MongoDB Version 
 variable "mongodb_version" {
   type        = string
   description = "MongoDB Version"
+  default = "6.0"
 }
 
 # IP Address Access
 variable "ip_address" {
   type = string
   description = "IP address used to access Atlas cluster"
+  default = "179.222.164.199"
 }
 
 # AWS Region
 variable "aws_region" {
   type = string
   description = "AWS Region"
+  default = "us-east-2"
 }
