931592f2341167aa0193c0a390a4976f9e516a69
t-tozatto <thais.c.tozatto@gmail.com>
2024-05-19 08:36:48 -0300

    output

--- catalog/output.tf
+++ catalog/output.tf
@@ -1,5 +1,4 @@
+
 output "master_password" {
-  value       = join("", aws_docdb_cluster.cluster[*].master_password)
-  description = "Password for the master DB user"
-  sensitive   = false
-}
+      value = nonsensitive(join("", aws_docdb_cluster.cluster[*].master_password))
+    }
